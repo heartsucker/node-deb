ffe5645763355b0d8a28358826a06869b6c2d89f
Heartsucker <ehartsuyker@gmail.com>
2015-08-29 14:17:31 +0200

    slightly smarter tests

--- test.sh
+++ test.sh
@@ -10,6 +10,11 @@ finish() {
 
 trap "finish" EXIT
 
+die() {
+  echo "$@" >&2
+  exit 1
+}
+
 test-simple-project() {
   echo "Running tests for simple-project"
   cd "$_pwd/test/simple-project"
@@ -20,8 +25,17 @@ test-simple-project() {
 test-whitespace-project() {
   echo "Running tests for whitespace-project"
   cd "$_pwd/test/whitespace-project"
-  ../../node-deb 'whitespace file.js' 'whitespace folder'
-  ../../node-deb whitespace\ file.js whitespace\ folder
+
+  output=`../../node-deb 'whitespace file.js' 'whitespace folder' 2>&1`
+  output+='\n'
+  output+=`../../node-deb whitespace\ file.js whitespace\ folder 2>&1`
+
+  if [[ $output == '*No such file or directory*' ]]; then
+    echo 'There was an error with the test.' >&2
+    echo -e "$output" >&2
+    die 'Unable to locate a directory. This is likely an error with `find`.'
+  fi
+
   echo "Success for whitespace-project"
 }
 
