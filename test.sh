3ec139d158083deddb91edfebe18c36a70ad1bc8
heartsucker <heartsucker@autistici.org>
2017-03-22 22:09:38 +0100

    moved generic tests to new test suite

--- test.sh
+++ test.sh
@@ -13,6 +13,13 @@ declare -ar all_tests=('simple-project')
 cur_dir="$(dirname $(readlink -f $0))"
 declare -r cur_dir
 
+echo 'Running generic checks (no docker)'
+echo 'Running CLI checks'
+PAGER=cat ./node-deb --help           > /dev/null
+PAGER=cat ./node-deb --show-readme    > /dev/null
+PAGER=cat ./node-deb --show-changelog > /dev/null
+echo 'Done running generic checks'
+
 for image in "${all_versions[@]}"; do
   for tst in "${all_tests[@]}"; do
     docker run --rm \
