17ef7c419cd1ab9cb5ddb0e019397fa7073675ac
heartsucker <heartsucker@autistici.org>
2017-10-06 16:21:53 +0200

    skip creating/removing users/groups when init == none

--- test/no-init/test.sh
+++ test/no-init/test.sh
@@ -13,8 +13,25 @@ finish() {
 ../../../node-deb --verbose \
                   -- app.sh
 
+list_groups() {
+  cut -d: -f1 /etc/group | sort
+}
+
+list_users() {
+  cut -d: -f1 /etc/passwd | sort
+}
+
+old_groups=$(list_groups)
+declare -r old_groups
+
+old_users=$(list_users)
+declare -r old_users
+
 dpkg -i "$output.deb"
 sleep 1
+
+[[ "$(list_groups)" == "$old_groups" ]] || die 'Groups unequal'
+[[ "$(list_users)" == "$old_users" ]] || die 'Users unequal'
 
 declare -r target_file='/var/log/no-init/TEST_OUTPUT'
 
